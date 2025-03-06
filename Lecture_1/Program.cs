9e559952973e20ae34d62676c5a30e15fab5327b
Mariami <70816127+mtkebuch@users.noreply.github.com>
2025-03-06 19:47:47 +0400

    Add files via upload

---
+++ Lecture_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Lecture_1
+{
+    internal class Program
+    {
+        static string connectionString = "Server = LAB321PC08\\SQLEXPRESS02; Database=ClothingStoreDB;Trusted_Connection=True;";
+        static void Main(string[] args)
+        {
+            //SqlConnection conn = new SqlConnection(connectionString);
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO [dbo].[Categories]([CategoryName]) VALUES(<CategoryName, varchar(100),>)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+
+
+
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
